af7ef816c67ea86a8e14560a7fcb6aa2e111eed4
Lee <lemanhhiep96@gmail.com>
2023-01-02 08:04:35 +0700

    Changes to be committed: 	modified:   Angular-practice/my-second-angular-project/src/app/app.component.html 	modified:   Angular-practice/my-second-angular-project/src/app/app.component.ts 	modified:   Angular-practice/my-second-angular-project/src/app/app.module.ts 	new file:   Angular-practice/my-second-angular-project/src/app/server/server.component.html 	new file:   Angular-practice/my-second-angular-project/src/app/server/server.component.ts 	new file:   Angular-practice/my-second-angular-project/src/app/servers/servers.component.css 	new file:   Angular-practice/my-second-angular-project/src/app/servers/servers.component.html 	new file:   Angular-practice/my-second-angular-project/src/app/servers/servers.component.spec.ts 	new file:   Angular-practice/my-second-angular-project/src/app/servers/servers.component.ts 	modified:   Angular-practice/my-second-angular-project/src/index.html

--- Angular-practice/my-second-angular-project/src/app/app.component.ts
+++ Angular-practice/my-second-angular-project/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //Khao bao selector cho component, giong nhu the root-app trong file index.html
   selector: 'app-root',
+  //Khai bao file html ma conponent dai dien
   templateUrl: './app.component.html',
+  //Khai bao file style.css ma component nay se su dung
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
